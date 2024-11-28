9f5ff9a45d9121b17cef5b252f1c9604cfb8ca39
KakashiHatake365 <74249501+KakashiHatake365@users.noreply.github.com>
2024-11-27 19:30:32 -0500

    Add files via upload

---
+++ JobPostingApplication/services/JobApplicationService.java
@@ -0,0 +1,64 @@
+package com.humber.JobPostingApplication.services;
+
+import com.humber.JobPostingApplication.models.Application;
+import com.humber.JobPostingApplication.repositories.ApplicationRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class JobApplicationService {
+    private final ApplicationRepository applicationRepository;
+
+    public JobApplicationService(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+    //Save a new job Application
+    public Application saveJobApplication(Application application) {
+        return applicationRepository.save(application);
+    }
+
+    //Get All Job application
+    public List<Application> getAllJobApplications() {
+        return applicationRepository.findAll();
+    }
+
+    //Get job applications by user (seeker)
+    public List<Application> getJobApplicationByUser(Long userId) {
+        return applicationRepository.findByUserId(userId);
+    }
+
+    //get job applications for specific job
+    public List<Application> getJobApplicationByJob(Long jobId) {
+        return applicationRepository.findByJobId(jobId);
+    }
+
+    //Get job application by id
+    public Optional<Application> getJobApplicationById(Long id) {
+        return applicationRepository.findById(id);
+    }
+
+    //Update an existing job application
+    public Application updateJobApplication(Long id, Application updatedApplication) {
+        //Check if the job application exists
+        Optional<Application> existingApplication = applicationRepository.findById(id);
+        if (existingApplication.isPresent()) {
+            Application applicationToUpdate = existingApplication.get();
+            applicationToUpdate.setCoverLetter(updatedApplication.getCoverLetter());
+            applicationToUpdate.setResume(updatedApplication.getResume());
+            // Other fields to update can be added here
+            return applicationRepository.save(applicationToUpdate);
+        }
+        throw new RuntimeException("Job application not found");
+    }
+
+    // Delete a job application by id
+    public int deleteJobApplicationById(Long id) {
+        if (applicationRepository.existsById(id)) {
+            applicationRepository.deleteById(id);
+            return 1;
+        }
+        // Return failure code if job application is not found
+        return -1;
+    }
+}
