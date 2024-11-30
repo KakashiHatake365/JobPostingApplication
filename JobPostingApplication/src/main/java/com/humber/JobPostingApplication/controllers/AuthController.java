a3aa9b7ccfdcd2b0e6f63957349c62882a7a97e5
KakashiHatake365 <74249501+KakashiHatake365@users.noreply.github.com>
2024-11-30 17:37:44 -0500

    Update AuthController.java

--- JobPostingApplication/src/main/java/com/humber/JobPostingApplication/controllers/AuthController.java
+++ JobPostingApplication/src/main/java/com/humber/JobPostingApplication/controllers/AuthController.java
@@ -1,18 +1,62 @@
 package com.humber.JobPostingApplication.controllers;
 
+import com.humber.JobPostingApplication.models.MyUser;
+import com.humber.JobPostingApplication.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AuthController {
-    private final
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //error endpoint
+    @GetMapping("/error")
+    public String error() {
+        return "fragments/auth/error";
+    }
+
+    //login endpoint
     @GetMapping("/login")
     public String login(@RequestParam(required = false) String message, Model model) {
         model.addAttribute("message", message);
 
         return "fragments/auth/login";
+    }
+
+    //Logout endpoint
+    @GetMapping("/logout")
+    public String customLogout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        //logout logic
+        new SecurityContextLogoutHandler().logout(request, response, authentication);
+        return "redirect:/login?message=You have been logged out!";
+    }
+
+    //register endpoint (open to registration view
+    @GetMapping("/register")
+    public String register(Model model, @RequestParam String message) {
+        model.addAttribute("message", message);
+        model.addAttribute("user", new MyUser()); //creating a new user under the MyUser variable
+        return "fragments/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@ModelAttribute MyUser user) {
+        //saves the user to the database
+        int saveUserCode = userService.saveUser(user);
+
+        if (saveUserCode == 0) {
+            return "redirect:/register?message=Registration Failed! Username already exists!";
+        } else {
+            return "redirect:/login?message=Registration Successful!";
+        }
     }
 }
